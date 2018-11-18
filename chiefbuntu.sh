90032e9667565a398d69f16b20bd3851f27c9837
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2018-11-18 06:32:05 +0100

    steps

---
+++ chiefbuntu.sh
@@ -0,0 +1,15 @@
+# setup script 4 ubuntu desktop 18.04
+
+# remove unnecessary ubuntu packages
+
+# connect to wifi
+
+# update apt
+
+# install curl
+
+# install node-install
+
+# install node + npm
+
+# ...
